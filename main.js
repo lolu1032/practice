779b8793b77cacba1af54bf5bb65fee2e4f52f50
lolu1023 <soowkw@naver.com>
2021-08-30 00:34:12 +0900

    슬라이드

---
+++ main.js
@@ -0,0 +1,13 @@
+
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+        delay: 3000
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
